92a8a84a6d7aea1531c6d38bdf68d8e406a4dc8e
Mats <matjoh50@gmail.com>
2024-10-25 12:56:11 +0200

    Moonmission compleate and outcommented.

--- moonbase.sql
+++ moonbase.sql
@@ -1,4 +1,4 @@
-# --Uppgift 1: Create Succesfull mission table
+-- Uppgift 1: Create Succesfull mission table
 # CREATE TABLE succesfull_mission AS
 #     SELECT moon_mission.mission_id,
 #            moon_mission.spacecraft,
@@ -10,11 +10,11 @@
 #     FROM moon_mission
 #     WHERE moon_mission.outcome = 'Successful';
 
-#--Uppgift 2:
+-- Uppgift 2:
 # ALTER TABLE succesfull_missions AUTO_INCREMENT = 1;
 # ALTER TABLE succesfull_missions ADD PRIMARY KEY (mission_id);
 
-#--Uppgift 3:
+-- Uppgift 3:
 # UPDATE moon_mission
 # SET operator = REPLACE(operator, ' ', '')
 # WHERE operator IS NOT NULL;
@@ -22,4 +22,42 @@
 # UPDATE succesfull_missions
 # SET operator = REPLACE(operator,' ', '')
 # WHERE operator IS NOT NULL;
+
+-- Uppgift 4:
+# DELETE FROM succesfull_missions
+# WHERE launch_date >= '2010-01-01';
+
+-- Uppgift 5:
+## 5. Gör en SELECT för att ta ut samtliga rader och kolumner från tabellen “account”, men slå ihop
+## ‘first_name’ och ‘last_name’ till en ny kolumn ‘name’, samt lägg till en extra kolumn ‘gender’ som
+## du ger värdet ‘female’ för alla användare vars näst sista siffra i personnumret är jämn, och värdet
+## ‘male’ för de användare där siffran är udda.
+
+# ALTER TABLE account
+# ADD gender VARCHAR(255);
+#
+# UPDATE account
+# SET gender = CASE
+#                  WHEN account.gender IS NULL AND
+#                       CAST(SUBSTRING(ssn, LENGTH(ssn), 1) AS UNSIGNED) % 2 = 0 THEN 'Female'
+#                  ELSE 'Male'
+#     END
+# WHERE account.gender IS NULL;
+#
+# SELECT *,
+#        CONCAT(first_name, ' ', last_name) AS name
+# FROM account;
+
+-- Uppgift 6
+
+# DELETE FROM account
+# WHERE gender = 'Female'
+#     AND CAST(SUBSTRING(ssn, 1, 2) AS UNSIGNED) < 70;
+
+-- Uppgift 7
+# SELECT gender,
+# FLOOR(YEAR(CURDATE()) - AVG(CAST(CONCAT('19',SUBSTRING(ssn, 1, 2)) AS UNSIGNED))) AS avg_age
+# FROM account
+# GROUP BY gender;
+
 
